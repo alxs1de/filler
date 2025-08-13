c3e07647bf304590b2a48c20e05d98395ff24bbc
alxs1de <alxside7@gmail.com>
2025-08-13 13:45:03 +0200

    blin

--- test_api.py
+++ test_api.py
@@ -7,49 +7,61 @@ import pytest
 import allure
 import requests
 
-token = '20eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NTQ1OTQ5MzgsImlhdCI6MTc1NDQyNjkzOCwiaXNzIjoiL2FwaS92MS9hdXRoL2Fub255bW91cyIsInN1YiI6ImQ5OWYzMzliZWMzYmU4NzNmYzE2NDhjYTNkZjhiODIzM2E5NjVlNjA4MDQ1ZTc2OTRlYjU5MDZjOWQ5ZTVmNTQiLCJ0eXBlIjoxMH0.koKtfCCldwSXW6uhdzu6nOqL2Y5JadqGCtnaRDHh0k8'
+token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NTUyNTIxOTgsImlhdCI6MTc1NTA4NDE5OCwiaXNzIjoiL2FwaS92MS9hdXRoL2Fub255bW91cyIsInN1YiI6IjhiNDk3NGI3ODdjNzgxMGZmMjc0MzU4ZDFmZTY3Nzk4ZjIxNGIwNjQ5ZWNlZDc5MzU1YTI0YWRlNjM0MTBkN2IiLCJ0eXBlIjoxMH0.aD74B81UCJPg9mp_H5153HLpyWrtBBz7E5XyJ0_3-4M'
 headers = {
     "Authorization": f"Bearer {token}",
     "Content-Type": "application/json"
 }
 
-def test_put():
+# def test_get_numbers_degrees():
+#
+#     url = 'https://web-agr.chitai-gorod.ru/web/api/v2/search/facet-search?customerCityId=213&phrase=451%C2%B0%20%D0%BF%D0%BE%20%D0%A4%D0%B0%D1%80%D0%B5%D0%BD%D0%B3%D0%B5%D0%B9%D1%82%D1%83&abTestGroup=1'
+#     response = requests.get(url, headers=headers)
+#
+#     assert response.status_code == 200
 
-    url = 'https://web-agr.chitai-gorod.ru/web/api/v2/search/facet-search?customerCityId=213&phrase=Solo%20Leveling&abTestGroup=1'
-    body = {"title": "ГосУслуги"}
-    response = requests.put(url, headers=headers, json=body)
+# def test_get_empty():
+#
+#     url = 'https://web-agr.chitai-gorod.ru/web/api/v2/search/facet-search?customerCityId=213&phrase=&abTestGroup=1'
+#     response = requests.get(url, headers=headers)
+#
+#     assert response.status_code == 403
+#
+#
+# def test_get_cirillic():
+#
+#     url = 'https://web-agr.chitai-gorod.ru/web/api/v2/search/facet-search?customerCityId=213&phrase=%D0%9F%D1%80%D0%B5%D1%81%D1%82%D1%83%D0%BF%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5%20%D0%B8%20%D0%9D%D0%B0%D0%BA%D0%B0%D0%B7%D0%B0%D0%BD%D0%B8%D0%B5&abTestGroup=1'
+#     response = requests.get(url, headers=headers)
+#
+#     assert response.status_code == 200
+#
+# def test_get_latin():
+#
+#     url = 'https://web-agr.chitai-gorod.ru/web/api/v2/search/facet-search?customerCityId=213&phrase=War%20and%20Peace&abTestGroup=1'
+#     response = requests.get(url, headers=headers)
+#
+#     assert response.status_code == 200
+#
+# def test_get_hieroglyphs():
+#
+#     url = 'https://web-agr.chitai-gorod.ru/web/api/v2/search/facet-search?customerCityId=213&phrase=%E0%A4%B0%E0%A4%95%E0%A5%8D%E0%A4%A4%20%E0%A4%AE%E0%A5%87%E0%A4%B0%E0%A4%BF%E0%A4%A1%E0%A4%BF%E0%A4%AF%E0%A4%A8&abTestGroup=1'
+#     response = requests.get(url, headers=headers)
+#
+#     assert response.status_code == 200
 
-    assert response.status_code == 200
-
-def test_get_empty():
-
-    url = 'https://web-agr.chitai-gorod.ru/web/api/v2/search/facet-search?customerCityId=213&phrase=Solo%20Leveling&abTestGroup=1'
-    body = {"title": ""}
-    response = requests.get(url, headers=headers, json=body)
-
-    assert response.status_code == 200
+### ДРУГИЕ ТЕСТЫ (МЕТОДЫ PRODUCT И SEMANTIC)
 
 
-def test_get_cirillic():
-
-    url = 'https://web-agr.chitai-gorod.ru/web/api/v2/search/facet-search?customerCityId=213&phrase=Solo%20Leveling&abTestGroup=1'
-    body = {"title": "привет"}
-    response = requests.get(url, headers=headers, json=body)
-
-    assert response.status_code == 200
-
-def test_get_latin():
-
-    url = 'https://web-agr.chitai-gorod.ru/web/api/v2/search/facet-search?customerCityId=213&phrase=Solo%20Leveling&abTestGroup=1'
-    body = {"title": "hi"}
-    response = requests.get(url, headers=headers, json=body)
-
-    assert response.status_code == 200
-
-def test_get_hieroglyphs():
-
-    url = 'https://web-agr.chitai-gorod.ru/web/api/v2/search/facet-search?customerCityId=213&phrase=Solo%20Leveling&abTestGroup=1'
-    body = {"title": "أهلاً"}
-    response = requests.get(url, headers=headers, json=body)
-
-    assert response.status_code == 200
+# def test_big_test():
+#
+#     url = 'https://web-agr.chitai-gorod.ru/web/api/v2/search/product?customerCityId=213&products%5Bpage%5D=1&products%5Bper-page%5D=60&phrase=good%20morning&abTestGroup=1'
+#     response = requests.get(url, headers=headers)
+#
+#     assert response.status_code == 200
+#
+# def test_bigger_test():
+#
+#     url = 'https://web-agr.chitai-gorod.ru/web/api/v1/recommend/semantic?phrase=good+morning&perPage=48'
+#     response = requests.get(url, headers=headers)
+#
+#     assert response.status_code == 200
